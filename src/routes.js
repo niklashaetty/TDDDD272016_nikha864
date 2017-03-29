7f4b35f219dfbbd7c9abe949aa34fe418e63c97a
Niklas Hätty <nikha864@student.liu.se>
2017-03-29 22:28:55 +0200

    Add 404 to routing

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import {Router, Route, browserHistory, IndexRoute } from 'react-router';
+
+// Error components
+import {NotFound} from './components/errorpages'
+
+// Components
 import Home from './components/home';
 import Login from './components/login';
+
 
 export default (
 	<Router history={browserHistory}>
 		<Route path="/" component={Home} />
-		<Route path="/login" component={Login} />
+		<Route path="/users/:id" component={Login} />
+		<Route path="*" component={NotFound} />
 	</Router>
 );
