0625aedd7fd8cbff0a4510cc373d016a77722987
Niklas Hätty <nikha864@student.liu.se>
2017-03-29 22:30:06 +0200

    Add support for header link to dashboard when logged in

--- src/components/header.js
+++ src/components/header.js
@@ -6,11 +6,25 @@ import FontAwesome from 'react-fontawesome'
 
 
 class Header extends Component {
+		constructor(props) {
+		super(props);
+		this.state = {user: null};
+	}
+
 	render() {
+		let display = null;
+		console.log(this.state.user);
+		if(this.state.user){
+			display = <p className="menu_item"><Link to='/:id' ><FontAwesome name="user-o"/> {this.state.user}</Link></p>;
+		}
+
 		return (
 			<div className="header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<div className="logo_text"> Project Name</div>
+				<div className="menu_right">
+					{display}
+				</div>
 			</div>
 		);
 	}
