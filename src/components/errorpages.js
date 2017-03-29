1ddda4a5d5a4217b78dfa1081a6c339e60b561e9
Niklas Hätty <nikha864@student.liu.se>
2017-03-29 22:50:12 +0200

    Add go home button to 404

--- src/components/errorpages.js
+++ src/components/errorpages.js
@@ -1,3 +1,5 @@
+/* Contains error pages such as 404, 503 '*/
+import {Link } from 'react-router';
 import React, {Component} from 'react';
 import '../css/errorpages.css'
 import Header from './header';
@@ -10,6 +12,8 @@ export class NotFound extends Component {
 				<div className="error_page">
 					<p className="not_found_big"> 404 </p>
 					<p className="not_found_small"> The page could not be found </p>
+					<Link to="/"><button className="submit">Go home</button>
+					</Link>
 				</div>
 			</div>
 		);
