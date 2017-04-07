068ee2672ec5e3ce2e68e7880ec196091093b552
Niklas Hätty <nikha864@student.liu.se>
2017-04-07 12:03:24 +0200

    Use bytea datatype for hash storing

--- database/schema.sql
+++ database/schema.sql
@@ -1,10 +1,13 @@
-create table if not exists users(
+drop table if exists tokens;
+drop table if exists users;
+
+create table users(
     username text not null unique,
-    password_hash varchar not null unique,
+    password_hash bytea not null unique,
     primary key(uid)
 );
 
-create table if not exists tokens(
+create table tokens(
     username text not null unique,
     token text not null unique,
     primary key(token)
