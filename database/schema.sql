6a8ac4163726a45bf5f1e33827ddbdfd862562b3
Niklas Hätty <nikha864@student.liu.se>
2017-04-10 16:11:14 +0200

    Remove token storage in database

--- database/schema.sql
+++ database/schema.sql
@@ -4,11 +4,5 @@ drop table if exists users;
 create table users(
     username text not null unique,
     password_hash bytea not null unique,
-    primary key(uid)
-);
-
-create table tokens(
-    username text not null unique,
-    token text not null unique,
-    primary key(token)
+    primary key(username)
 );
