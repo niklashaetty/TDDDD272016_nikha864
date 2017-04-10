76e81f347cd1bb5cd4d1d372d4035ec336f0e644
Niklas Hätty <nikha864@student.liu.se>
2017-04-10 20:47:01 +0200

    Add mockup of links to course plans

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -11,8 +11,47 @@ import Header from './header';
 import Auth from './auth';
 /////////////////////////////////////////////////////////////////////////////
 
-class Dashboard extends Component {
+class PlannerLink extends Component {
     render() {
+        return (
+          <div className="planner_link">
+              <a className="link_name">{this.props.name}</a>
+              <p className="delete_icon"><FontAwesome name="ban"/></p>
+          </div>
+        );
+    }
+}
+
+class Dashboard extends Component {
+
+    fillCourseplans () {
+        var plans = [
+            {name: 'testplan1'},
+            {name: 'testplan2'},
+            {name: 'testplan3'},
+            {name: 'testplan4'},
+            {name: 'testplan5'},
+            {name: 'testplan6'},
+            {name: 'testplan7'},
+            {name: 'testplan8'},
+            {name: 'testplan9'},
+            {name: 'testplan10'}
+        ];
+
+        let result = [];
+
+        for(let i = 0; i < plans.length; i++){
+            result.push(<PlannerLink name={plans[i].name}/>)
+        }
+        return result
+    }
+
+
+    render() {
+
+        let coursePlans = this.fillCourseplans();
+
+
         return (
           <div className="wrapper">
               <Header user={this.props.location.state.username}/>
@@ -20,6 +59,8 @@ class Dashboard extends Component {
                   <div className="dashboard_left">
                       <div className="dashboard_headline">Course plans</div>
                       <div className="dashboard_big">
+                          <div className="whitespace"> </div>
+                          {coursePlans}
                       </div>
 
                       <div className="whitespace"> </div>
@@ -33,7 +74,7 @@ class Dashboard extends Component {
                       <div className="dashboard_headline">Dashboard</div>
                       <div className="dashboard_small">
                           <Link to={{pathname: '/'}}>
-                          <p onClick={() => {Auth.logOut()}} className="logout"><FontAwesome name="sign-out"/>Log out</p>
+                              <p onClick={() => {Auth.logOut()}} className="logout"><FontAwesome name="sign-out" />Log out</p>
                           </Link>
                       </div>
                   </div>
