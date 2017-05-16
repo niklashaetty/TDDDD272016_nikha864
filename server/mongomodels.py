0dec0f158e4b3a3f5a9933c0c7e78a9aae305fbf
Niklas Hätty <nikha864@student.liu.se>
2017-05-16 17:48:29 +0200

    Add backend support for course adding

--- server/mongomodels.py
+++ server/mongomodels.py
@@ -7,6 +7,7 @@ Make sure these database credentials are stored as environment vars:
 """
 
 from pymongo import MongoClient
+import json
 import os
 
 # Global variables
@@ -80,16 +81,16 @@ def add_course_plan(plan_hash, name, owner, total_ects, advanced_ects, semesters
         return False
 
 
-def add_semester(plan_hash, semester_name):
+def add_semester(identifier, semester_name):
     """
     Add a new and empty semester to and existing plan in the database.
-    :param plan_hash: Unique plan identifier
+    :param identifier: Unique plan identifier
     :param semester_name: name of the semester.
     :return: Boolean success
     """
     collection = get_collection()
 
-    semesters = get_semesters(plan_hash)
+    semesters = get_semesters(identifier)
     if len(semesters) >= MAX_SEMESTERS:
         return False
     else:
@@ -104,30 +105,100 @@ def add_semester(plan_hash, semester_name):
             "semester": semester_name
         }
         semesters.append(new_semester)
-        collection.update({'plan_hash': plan_hash},
+        collection.update({'plan_hash': identifier},
                           {'$set': {'semesters': semesters}})
         return True
 
 
-def get_course_plan_owner(plan_hash):
+def add_course(identifier, periods, blocks, semester_name, course):
+    """
+    Add a new course to a given plan by taking out all the semesters, 
+    editing what we need to edit, then add the semesters back into the database.
+    :param identifier: Unique plan identifier
+    :param periods: List of periods, i.e. ['period1', 'period2'], or if only one period it will
+    look like this: ['period2', None]
+    :param blocks: List of blocks, i.e. ['1', '2'], or if only one period it will
+    look like this: ['2', None]
+    :param semester_name: Name of semester that the course is added to.
+    :param course: Course object in form of dict.
+    :return: 
+    """
+
+    collection = get_collection()
+    semesters = get_semesters(identifier)
+
+    # Look for correct semester, temporarily remove from list.
+    # Save index so we can put the semester back in the same place
+    # Duplicate code from get_semester() to avoid another database call (this should be faster, but uglier)
+    correct_semester = None
+    old_index = 0
+    for s in semesters:
+        if s['semester'] == semester_name:
+            correct_semester = s
+            semesters.remove(s)
+            break
+        old_index += 1
+
+    # Not found, should never happen though since backend already checked.
+    if not correct_semester:
+        return False
+
+    old_first_period = correct_semester[periods[0]]
+    new_course = {
+        "points": course['ects'],
+        "level": course['level'],
+        "name": course['name'],
+        "block": blocks[0],
+        "code": course['code']
+    }
+    old_first_period.append(new_course)
+    correct_semester[periods[0]] = old_first_period
+
+    # If period two exists, we'll add the course to that period too.
+    if periods[1]:
+        print(blocks[1])
+        old_second_period = correct_semester['period2']
+        new_course = {
+            "points": course['ects'],
+            "level": course['level'],
+            "name": course['name'],
+            "block": blocks[1],
+            "code": course['code']
+        }
+        old_second_period.append(new_course)
+        correct_semester[periods[1]] = old_second_period
+
+    # Done editing, add the edited semester back to the list of all semesters and update the db.
+    semesters.insert(old_index, correct_semester)
+    try:
+        collection.update({'plan_hash': identifier},
+                          {'$set': {'semesters': semesters}})
+        return True
+
+    except Exception as e:
+        print(e)
+        return False
+
+
+def get_course_plan_owner(identifier):
     """
     Return owner of a course plan with provided hash.
-    :param plan_hash: Unique plan identifier
+    :param identifier: Unique plan identifier
     :return: owner
     """
     collection = get_collection()
-    plan = collection.find_one({'plan_hash': plan_hash}, {'_id': 0, 'owner': 1})
+    plan = collection.find_one({'plan_hash': identifier}, {'_id': 0, 'owner': 1})
     return plan['owner']
 
 
-def get_course_plan(plan_hash):
+def get_course_plan(identifier):
     """
     Find and get a course plan with provided hash.
-    :param plan_hash: Unique plan identifier
+    :param identifier: Unique plan identifier
     :return: Plan in json format, or None is no such plan exists
     """
     collection = get_collection()
-    course_plan = collection.find_one({'plan_hash': plan_hash}, {'_id': 0})
+    course_plan = collection.find_one({'plan_hash': identifier}, {'_id': 0})
     return course_plan
 
 
@@ -148,15 +219,41 @@ def get_all_plans(owner):
     return course_plans
 
 
-def get_semesters(plan_hash):
+def get_semesters(identifier):
     """
     Get all semesters with a provided plan_hash
-    :param plan_hash: Unique plan identifier
+    :param identifier: Unique plan identifier
     :return: List of semesters.
     """
     collection = get_collection()
-    res = collection.find_one({'plan_hash': plan_hash}, {'semesters': 1, '_id': 0})
+    res = collection.find_one({'plan_hash': identifier}, {'semesters': 1, '_id': 0})
     return res['semesters']
+
+
+def get_course_count(identifier, semester_name):
+    """
+    Get the amount of courses in a given semester
+    :param identifier: Unique plan identifier
+    :param semester_name: Name of semester
+    :return: Integer count
+    """
+    semester = get_semester(identifier, semester_name)
+    return len(semester['period1']) + len(semester['period2'])
+
+
+def get_semester(identifier, semester_name):
+    """
+    Get a semester with a provided identifier and name
+    :param identifier: Unique plan identifier
+    :param semester_name: name of semester
+    :return: Correct semester.
+    """
+    semesters = get_semesters(identifier)
+    for s in semesters:
+        if s['semester'] == semester_name:
+            return s
+
+    return None
 
 
 def plan_exists(identifier):
